0a9abc515cde4f55c04c658be06a652c5604b4f1
Chris <cwei1994@gmail.com>
2017-03-19 18:40:32 -0400

    added test job

--- urbansense-client/schema.sql
+++ urbansense-client/schema.sql
@@ -1,5 +1,8 @@
 drop table if exists test;
 create table test (
     id integer primary key autoincrement,
-    time integer not null
+    time integer not null,
+    lat real not null,
+    lng real not null,
+    value real not null
 );
--- urbansense-client/urbansense_client.py
+++ urbansense-client/urbansense_client.py
@@ -1,7 +1,10 @@
 import os
+import time
+from sqlite3 import dbapi2 as sqlite3
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 from .exceptions import InvalidUsage
+from influxdb import InfluxDBClient
 
 app = Flask(__name__)
 
@@ -16,6 +19,8 @@ app.config.update(dict(
     INFLUX_PORT=8086
 ))
 
+def get_influx_client():
+    return InfluxDBClient(app.config['INFLUX_HOST'], app.config['INFLUX_PORT'], database='alpha')
 
 def connect_db():
     """Connects to the specific database."""
@@ -38,6 +43,14 @@ def initdb_command():
     init_db()
     print('Initialized the database...')
 
+def get_db():
+   """Opens a new database connection if there is none yet for the
+   current application context.
+   """
+   if not hasattr(g, 'sqlite_db'):
+       g.sqlite_db = connect_db()
+   return g.sqlite_db
+
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
@@ -49,7 +62,25 @@ def handle_invalid_usage(error):
 def hello_world():
 	return 'UrbanSense server ready to receive data...\n'
 
+@app.route('/map')
+def render_test_map():
+    db = get_db()
+    db.execute("SELECT * FROM test")
+    result = db.fetchall()
+    return render_template("index.html", data=result)
 
+@app.route('/test')
 def test_task():
-    pass
-    
+    """Polls for 10 minutes of data every 10 minutes and selects the maximum
+    value in that time period"""
+    current_time = int(time.time())
+    client = get_influx_client()
+    results = client.query('''SELECT * FROM "ir" WHERE "time" <= %d AND "time" > %d''' % (current_time, current_time - 60*10))
+    pts = list(results.get_points(measurement="ir"))
+    m = max(pts, key=lambda x: x["value"])
+    # insert max result into our sqlite database
+    db = get_db()
+    db.execute("INSERT INTO test (time, lat, lng, value) VALUES (?, ?, ?, ?)",
+        (m["time"], m["lat"], m["lng"], m["value"]))
+    db.commit()
+    return "success!"
